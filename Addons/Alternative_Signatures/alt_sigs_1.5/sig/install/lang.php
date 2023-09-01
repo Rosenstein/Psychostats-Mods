e1ef73bcfbf4689486c0ad2b3bef83df282739ac
Rosenstein <Rosenstein@users.noreply.github.com>
2023-09-01 04:03:12 +0200

    Add alternative signatures 1.5

---
+++ Addons/Alternative_Signatures/alt_sigs_1.5/sig/install/lang.php
@@ -0,0 +1,53 @@
+<?php
+$lang01['en'] = "Installing manual";
+$lang01['ru'] = "Инструкция по установке";
+$lang01['sv'] = "Installera manuellt";
+
+$lang02['en'] = "Open/create .htaccess file in your PsychoStats directory ";
+$lang02['ru'] = "Откройте/создайте файл .htaccess в папке PsychoStats ";
+$lang02['sv'] = "Цppna/skapa .htaccess filen i din Psychostats katalog ";
+
+$lang03['en'] = "Add these lines in .htaccess at the beginning: (on a blank line)";
+$lang03['ru'] = "Добавьте эти строки в .htaccess в самом начале: (на чистой строке)";
+$lang03['sv'] = "Lдgg till dessa rader i bцrjan av din .htaccess fil (pе en blank rad)";
+
+if ($_POST['ver'] == "3.1") {
+$lang04['en'] = "Open player_profile.html file in ";
+$lang04['ru'] = "Откройте player_profile.html, расположенный в ";
+$lang04['sv'] = "Цppna player_profile.html (  ";
+} else {
+$lang04['en'] = "Open block_plrsessions.html file in ";
+$lang04['ru'] = "Откройте block_plrsessions.html, расположенный в ";
+$lang04['sv'] = "Цppna block_plrsessions.html ( ";
+}
+
+$lang05['en'] = "Add these lines at the ending: (on a blank line)";
+$lang05['ru'] = "Добавьте эти строки в самом конце: (на чистой строке)";
+$lang05['sv'] = "Och lдgg till dessa rader pе slutet: (pе en tom rad)";
+
+$lang06['en'] = "Alternative Signatures MOD is installed now!";
+$lang06['ru'] = "Alternative Signatures установлен!";
+$lang06['sv'] = "'Alternative Signatures MOD' дr nu installerat!";
+
+$lang07['en'] = "Your player's pages should now look like this:";
+$lang07['ru'] = "Теперь страницы игроков должны выглядеть так:";
+$lang07['sv'] = "Spelar Profilerna kommer nu att se ut sе hдr:";
+
+$lang08['en'] = "Don't forget to remove install directory!";
+$lang08['ru'] = "Не забудьте удалить установочную директорию!";
+$lang08['sv'] = "Glцm inte att ta bort 'install' katalogen!!";
+
+if ($_POST['ver'] == "3.1") {
+$lang09['en'] = "Open overall.js file in";
+$lang09['ru'] = "Откройте overall.js, расположенный в";
+$lang09['sv'] = "Цppna overall.js (";
+} else {
+$lang09['en'] = "Open webcore.js file in";
+$lang09['ru'] = "Откройте webcore.js, расположенный в";
+$lang09['sv'] = "Цppna webcore.js (";
+}
+
+$lang10['en'] = "steps";
+$lang10['ru'] = "шага";
+$lang10['sv'] = "steg";
+?>
